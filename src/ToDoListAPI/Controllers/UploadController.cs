423d1cf16aafe75367126941058a392d382de147
taeyo <taeyokim@gmail.com>
2017-08-14 20:48:12 +0900

    프로젝트 초기 업로드

---
+++ src/ToDoListAPI/Controllers/UploadController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using ToDoListAPI.Providers;
+
+namespace ToDoListAPI.Controllers
+{
+    [RoutePrefix("api/upload")]
+    public class UploadController : ApiController
+    {
+        private const string Container = "images";
+
+        [HttpPost, Route("")]
+        public async Task<IHttpActionResult> UploadFile()
+        {
+            if (!Request.Content.IsMimeMultipartContent())
+            {
+                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+            }
+
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["StorageConnectionString"]);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            CloudBlobContainer imagesContainer = blobClient.GetContainerReference(Container);
+            imagesContainer.CreateIfNotExists(BlobContainerPublicAccessType.Blob);
+            var provider = new AzureStorageMultipartFormDataStreamProvider(imagesContainer);
+
+            try
+            {
+                await Request.Content.ReadAsMultipartAsync(provider);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"An error has occured. Details: {ex.Message}");
+            }
+
+            // Retrieve the filename of the file you have uploaded
+            var filename = provider.FileData.FirstOrDefault()?.LocalFileName;
+            if (string.IsNullOrEmpty(filename))
+            {
+                return BadRequest("An error has occured while uploading your file. Please try again.");
+            }
+
+            return Ok($"File: {filename} has successfully uploaded");
+        }
+    }
+}
